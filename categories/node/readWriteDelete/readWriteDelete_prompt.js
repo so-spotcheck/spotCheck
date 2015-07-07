2b91138d5ad7cfb31c755b19b1b6b06fee0adda8
Ansel Rosenberg <ansel01@gmail.com>
2015-07-06 19:58:02 -0700

    Add problem and solution for Node filesystem. Closes #14
    Problem asks student to read content from one file and append it to another file with out deleting any content in the second file, then delete the original file. All very solvable with  Node's 'filesystem' module.

---
+++ categories/node/readWriteDelete/readWriteDelete_prompt.js
@@ -0,0 +1,4 @@
+// Write the necessary code to:
+// a) read the contents from 'sample-read.txt'
+// b) write those contents to 'sample-write.txt' without deleting anything already there
+// c) then delete 'sample-read.txt'
