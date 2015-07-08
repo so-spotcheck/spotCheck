abaef3b348f96d96eea96667099e4a18f41ad841
Ansel Rosenberg <ansel01@gmail.com>
2015-07-07 22:15:43 -0700

    Add prompt for sql problem commonRows
    commonRows asks for basic SQL operations, including creating a database (using sqlite), creating tables, inserting into those tables, and writing a join function

---
+++ categories/sql/commonRows/commonRows_prompt.sql
@@ -0,0 +1,13 @@
+/*
+
+record the commands you would need to do the following steps from your terminal:
+
+create a new sqlite3 database called 'database1'
+create two tables, 'Cities' and 'States', that each have a column 'Name' which accepts strings
+insert three entries into 'Cities': San Francisco, Portland and New York
+insert three entries into 'States': California, Oregon and New York
+run a query that only shows only the shared fields between the two tables
+
+ */
+
+-- write steps below --
